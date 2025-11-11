cd4c844d6790ba42b3bdceba43144280a1127e19
Anandika-chembakasseri <anandikachembakasseri@gmail.com>
2025-11-11 14:46:38 +0530

    new

--- proapp/urls.py
+++ proapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from.import views
 urlpatterns = [
-    path ('',views.new)
+    path ('',views.new),
+    path ('temp',views.new1)
 ]
--- proapp/views.py
+++ proapp/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 
 def new(request):
     return render(request,'login.html')
+def new1(request):
+    return render(request,'temp.html')
 
 # Create your views here.
